8f32304660e52f3a3c4f3ae8c6a43252f0e20ce4
Yevhen Lehenkyi <meister.ramires@gmail.com>
2024-06-04 23:03:54 +0300

    task 1-3 done

---
+++ js/task-03.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+    let filtered = [];
+    for (const item of numbers) {
+        if (item > value) {
+            filtered.push(item);
+        } 
+    } return filtered
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
